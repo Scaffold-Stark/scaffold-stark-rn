1f25e678f4fcac9512a415088657560e1f422f4b
metalboyrick <rsulisthio@gmail.com>
2025-09-17 18:01:25 +0700

    feat: add global font configs

--- packages/rn/app/_layout.tsx
+++ packages/rn/app/_layout.tsx
@@ -6,7 +6,7 @@ import "../global.css";
 export default function RootLayout() {
   return (
     <ScaffoldStarkAppWithProviders>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
       <ToastManager />
     </ScaffoldStarkAppWithProviders>
   );
