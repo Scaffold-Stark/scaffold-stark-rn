1002abd8a7fb819a8b36c4e9e0e77e4f1eae6b81
tu11aa <thanhtuhr01@gmail.com>
2025-09-22 16:53:58 +0700

    STRK icon

--- packages/rn/app/(tabs)/hooks.tsx
+++ packages/rn/app/(tabs)/hooks.tsx
@@ -1,17 +1,24 @@
 // THIS IS A PAGE TO TEST HOOKS
 // TODO: DELETE THIS FILE AFTER TESTING
 
+import { StarkIcon } from "@/components/scaffold-stark/icons/StarkIcon";
+import {
+  themeColors,
+  useTheme,
+} from "@/components/scaffold-stark/ThemeProvider";
 import { useDeployedContractInfo } from "@/hooks/scaffold-stark/useDeployedContractInfo";
 import { useScaffoldEventHistory } from "@/hooks/scaffold-stark/useScaffoldEventHistory";
 import { useScaffoldMultiWriteContract } from "@/hooks/scaffold-stark/useScaffoldMultiWriteContract";
 import { useScaffoldReadContract } from "@/hooks/scaffold-stark/useScaffoldReadContract";
 import { useScaffoldWriteContract } from "@/hooks/scaffold-stark/useScaffoldWriteContract";
 import { useAccount } from "@starknet-react/core";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { CairoOption, CairoOptionVariant } from "starknet";
 
 export default function Hooks() {
   const { address } = useAccount();
+  const { theme, isDark } = useTheme();
+  const colors = themeColors[theme];
 
   const YourContract = useDeployedContractInfo("YourContract");
 
@@ -62,6 +69,27 @@ export default function Hooks() {
 
   return (
     <View>
+      <View className="items-center justify-center">
+        <View
+          className="rounded-full items-center justify-center mb-8 p-3"
+          style={{
+            backgroundColor: isDark ? colors.primary + "25" : "#ffffff" + "25",
+          }}
+        >
+          <View
+            className="rounded-full items-center justify-center p-4"
+            style={{
+              backgroundColor: isDark
+                ? colors.primaryLight + "50"
+                : "#ffffff" + "50",
+            }}
+          >
+            {/* Ionicons Home Icon */}
+            <StarkIcon />
+          </View>
+        </View>
+      </View>
+
       <Text>Hooks</Text>
       <Text>Greeting: {greeting?.toString()}</Text>
       <Text>Allowance: {allowance?.toString()}</Text>
