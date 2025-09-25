3a1931daaa2b6033bf227120ec2600bb401f0f41
tu11aa <thanhtuhr01@gmail.com>
2025-09-25 22:50:30 +0700

    refactor: update aegis config

--- packages/rn/configs/aegisConfig.ts
+++ packages/rn/configs/aegisConfig.ts
@@ -1,7 +1,15 @@
+import scaffoldConfig from "@/scaffold.config";
+
+const currentNetwork = scaffoldConfig.targetNetworks[0].network as string;
+
 const aegisConfig = {
-  network: "SN_SEPOLIA" as const,
+  network:
+    currentNetwork === "sepolia"
+      ? ("SN_SEPOLIA" as const)
+      : ("SN_DEVNET" as const),
   appName: "Starknet",
   appId: "app-39c5deb769927493723d734a3f500812",
+  paymasterApiKey: process.env.EXPO_PUBLIC_AVNU_API_KEY || undefined,
   enableLogging: true,
 };
 
