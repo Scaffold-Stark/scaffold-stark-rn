c436270091da6b66baf2e79f3a03d7480299a4ba
tu11aa <thanhtuhr01@gmail.com>
2025-09-26 14:36:00 +0700

    chore: setting coming soon

--- packages/rn/app/(tabs)/settings.tsx
+++ packages/rn/app/(tabs)/settings.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Settings() {
   return (
-    <View>
-      <Text>settings</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-center">Coming Soon</Text>
     </View>
   );
 }
